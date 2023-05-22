3ff0ab8a844417a0343b3ad8794f7f4867ba9771
JoaoBianco <joaops.bianco@gmail.com>
2023-05-22 17:57:13 -0300

    Install packages and create types

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "@squidit/css/dist/css/squid.min.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
---
+++ types.d.ts
@@ -0,0 +1,25 @@
+export type SquidResponse = {
+  uid: string;
+  link: string;
+  upvotes: number;
+  comentarios: number;
+  criadoEm: string;
+  legenda: string;
+  imagens: {
+    resolucaoPadrao: {
+      url: string;
+      width: number;
+      height: number;
+    };
+    resolucaoMedia: {
+      url: string;
+      width: number;
+      height: number;
+    };
+    thumbnail: {
+      url: string;
+      width: number;
+      height: number;
+    };
+  };
+}[];
